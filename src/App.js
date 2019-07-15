43c5b206919993781924d13d41b76734f370dece
alaa shaher <alaashaher12@gmail.com>
2019-07-15 14:24:32 +0200

    daymamic table

--- src/App.js
+++ src/App.js
@@ -3,61 +3,73 @@ import './App.css';
 import Tableclass from './component/table';
 const dataSource = [
   {
+    'id':1,
     'name': 'ahmed',
     'age': 32,
     'job': 'accounting',
   },
   {
+    'id':2,
     'name': 'mahmoud',
     'age': 23,
     'job': 'doctor',
   },
   {
+    'id':3,
     'name': 'esraa',
     'age': 23,
     'job': 'doctor',
   },
   {
+    'id':4,
     'name': 'eman',
     'age': 23,
     'job': 'doctor',
   },
   {
+    'id':5,
     'name': 'yasser',
     'age': 23,
     'job': 'doctor',
   },
   {
+    'id':6,
     'name': 'mohammed ',
     'age': 23,
     'job': 'doctor',
   },
   {
+    'id':7,
     'name': 'zayed',
     'age': 23,
     'job': 'doctor',
   },
   {
+    'id':8,
     'name': 'fatma',
     'age': 23,
     'job': 'doctor',
   },
   {
+    'id':9,
     'name': 'mariem',
     'age': 23,
     'job': 'doctor',
   },
   {
+    'id':10,
     'name': 'asmaa',
     'age': 23,
     'job': 'doctor',
   },
   {
+    'id':11,
     'name': 'faten',
     'age': 23,
     'job': 'doctor',
   },
   {
+    'id':12,
     'name': 'amany',
     'age': 23,
     'job': 'doctor',
@@ -65,7 +77,7 @@ const dataSource = [
 ];
 
 const columns =
-  ['name', 'age', 'job']
+  ['id','name', 'age', 'job']
   ;
 function App() {
   return (
